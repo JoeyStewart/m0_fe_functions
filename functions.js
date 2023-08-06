641825fbb646035a266a6801c48c6f678d6477dd
Joseph Stewart <JosephStewart1752@gmail.com>
2023-08-06 14:22:17 -0400

    Answers

--- functions.js
+++ functions.js
@@ -5,13 +5,25 @@
 
 // 1: Write a function named greeting that returns a string with a general greeting. 
 
+function greeting(){
+return "Howdy, partner!"
+}
+console.log(greeting())
 
 // 2: Write a function named customGreeting that returns a greeting WITH a specific name.
+
+function customGreeting(name){
+    return `Boy howdy ${name}, how are ya?`;
+    }
+    console.log(customGreeting("The Dude"))
 
 
 // 3: Write a function named greetPerson that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
-
+function greetPerson(first, middle, last){
+    return `Hey, how are you, ${first} ${middle} ${last}?`;
+}
+console.log(greetPerson("Theodore", "President", "Roosevelt"))
 // 4: Write a function named square that takes in one number, and returns the square of that number.
 // BONUS: Print a sentence that interpolates the return value of your square function.
 
